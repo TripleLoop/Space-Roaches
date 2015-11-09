39aa2f164bcb55462ced7d2f7776f1819e2d6ba6
Alberto <alberto.monte.gar@gmail.com>
2015-11-09 20:11:45 +0100

    (feat) Astronaut movement

--- Assets/_Scripts/Astronaut.cs
+++ Assets/_Scripts/Astronaut.cs
@@ -5,26 +5,29 @@ using System.Collections;
 public class Astronaut : MonoBehaviourEx, IHandle<UserInputMessage>
 {
 
-    #region Variables
+    private Rigidbody2D _rigidbody2D;
 
-        private Rigidbody2D _rigidbody2D;
+    private Vector2 _location;
+    private Vector2 _direction;
 
-        //Define Stats Machine's variables
+    public float Intensity;
+    private float _tempIntensity;
+    private float _boost;
 
-        public enum State
-        {
-            Idle,
-            Pulse,
-            Moving,
-            Die,
-        }
+    private Animator _animatorAst;
 
-        private Action _currentState;
-        public State CurrentStateName;
+    //Define Stats Machine's variables
 
-    #endregion
+    public enum State
+    {
+        Idle,
+        Pulse,
+        Moving,
+        Die,
+    }
 
-    #region state machine
+    private Action _currentState;
+    public State CurrentStateName;
 
     private void SetState(State state)
     {
@@ -37,9 +40,22 @@ public class Astronaut : MonoBehaviourEx, IHandle<UserInputMessage>
                 break;
             case State.Pulse:
                 _currentState = Pulse;
+                
+                _animatorAst.SetInteger("State", 1);
+
+                //Quaternion.Euler(0, 30, 0);
+
+                transform.rotation = Quaternion.FromToRotation(transform.parent.right, _direction);
+
+                //transform.rotation = Quaternion.Euler(0, 0, Quaternion.Angle(Quaternion.identity, Quaternion.Euler(new Vector3(_location.x, _location.y, 0)))); 
+                //Mathf.Rad2Deg * Mathf.Cos(1 / _direction.x));
+
+                _boost = 4.0f;
+                StartCoroutine(SlowDown());
                 break;
             case State.Moving:
                 _currentState = Moving;
+                _animatorAst.SetInteger("State", 0);
                 break;
             case State.Die:
                 _currentState = Die;
@@ -59,7 +75,7 @@ public class Astronaut : MonoBehaviourEx, IHandle<UserInputMessage>
 
                 break;
             case State.Pulse:
-
+                
                 break;
             case State.Moving:
 
@@ -70,41 +86,56 @@ public class Astronaut : MonoBehaviourEx, IHandle<UserInputMessage>
             default:
                 Debug.Log("unrecognized state");
                 break;
-
         }
     }
 
-#endregion
-
-    #region States
-
-        private void Idle()
-        {
-            
-        }
-
-        private void Pulse()
-        {
-        
-        }
-
-        private void Moving()
-        {
-        
-        }
-
-        private void Die()
-        {
-        
-        }
-
-    #endregion
-
-    // Use this for initialization
-    void Start()
+    private void Idle()
     {
-        _rigidbody2D = this.GetComponent<Rigidbody2D>();
-        _currentState = Idle;
+            
+    }
+
+    private void Pulse()
+    {
+        _tempIntensity = Intensity * _boost;
+        //transform.Translate(_direction * Time.deltaTime * _tempIntensity);
+        transform.Translate(Vector2.right * Time.deltaTime * _tempIntensity);
+    }
+
+    private void Moving()
+    {
+        //transform.Translate(_direction * Time.deltaTime * Intensity);
+        transform.Translate(Vector2.right * Time.deltaTime * Intensity);
+    }
+
+    private void Die()
+    {
+        
+    }
+
+    public void Handle(UserInputMessage message)
+    {
+        Debug.Log(message.Location);
+
+        _location = message.Location;
+
+        _direction.x = _location.x - transform.position.x;
+        _direction.y = _location.y - transform.position.y;
+        _direction.Normalize();
+
+        Debug.Log(_direction);
+
+        SetState(State.Pulse);
+    }
+
+    private IEnumerator SlowDown()
+    {
+        yield return new WaitForSeconds(1);
+        while (_boost > 1)
+        {
+            _boost -= 0.5f;
+            yield return new WaitForSeconds(0.2f);
+        }
+        SetState(State.Moving);
     }
 
     // Update is called once per frame
@@ -113,8 +144,12 @@ public class Astronaut : MonoBehaviourEx, IHandle<UserInputMessage>
         _currentState();
     }
 
-    public void Handle(UserInputMessage message)
+    // Use this for initialization
+    void Start()
     {
-        Debug.Log(message.Location);
+        _rigidbody2D = this.GetComponent<Rigidbody2D>();
+        _animatorAst = gameObject.GetComponent<Animator>();
+        Intensity = 0.5f;
+        _currentState = Idle;
     }
 }
