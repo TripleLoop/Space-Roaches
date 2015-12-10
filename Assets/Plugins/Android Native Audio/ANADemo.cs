aef0cbaccdcfda7012aa86c8a0f7baa7d47fb422
Marcel Barros Aparici <barros.marcel92@gmail.com>
2015-12-10 20:38:47 +0100

    (fix) reorganize and clean project

---
+++ Assets/Plugins/Android Native Audio/ANADemo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ANADemo : MonoBehaviour
+{
+	AudioSource unityAudio;
+	int soundID;
+	
+	void Start()
+	{
+		unityAudio = GetComponent<AudioSource>();
+		
+		AndroidNativeAudio.makePool(1);
+		soundID = AndroidNativeAudio.load("Android Native Audio/Tone Native.wav");
+	}
+
+	void OnGUI()
+	{
+		GUI.skin.button.fontSize = 32;
+		
+		if(GUI.Button(new Rect(20,40,680,100), "Unity Audio"))
+		{
+			unityAudio.Play();
+		}
+
+		if(GUI.Button(new Rect(20,640,680,100), "Native Audio"))
+		{
+			AndroidNativeAudio.play(soundID);
+		}
+	}
+}
