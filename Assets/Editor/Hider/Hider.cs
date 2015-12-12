ed4037bfe0ab2abc593f3d8f52e7a9812002aebb
Marcel Barros Aparici <barros.marcel92@gmail.com>
2015-12-12 19:30:33 +0100

    (fix) delete hidden gameobject

---
+++ Assets/Editor/Hider/Hider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+ 
+ public class Hider : EditorWindow
+{
+
+    [MenuItem("Window/GameObject_Hider&Destroyer/Start")]
+    public static void Create()
+    {
+        GetWindow<Hider>();
+    }
+
+    void OnGUI()
+    {
+
+        if (GUILayout.Button("Create hidden GO"))
+        {
+            GameObject h = new GameObject("hidden");
+            h.hideFlags = HideFlags.HideInHierarchy;
+        }
+        if (GUILayout.Button("Select hidden GO"))
+        {
+            Selection.activeGameObject = GameObject.Find("hidden");
+        }
+
+        if (GUILayout.Button("Destroy Selected Object"))
+        {
+            DestroyImmediate(Selection.activeObject);
+        }
+    }
+}
