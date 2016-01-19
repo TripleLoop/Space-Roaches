32b21c0e5bd14f6f59665aa5e39825b2ac1bd594
Marcel Barros Aparici <barros.marcel92@gmail.com>
2016-01-19 21:18:46 +0100

    (fix) added popup animation alert popup and adjusted popup animator

--- Assets/_Scripts/GUI/AlertPopUp.cs
+++ Assets/_Scripts/GUI/AlertPopUp.cs
@@ -1,16 +1,21 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class AlertPopUp : MonoBehaviourEx, IHandle<ShowAlertMessage>
 {
     private ChildrenControllerComponent _childrenController;
+    private Action _onCloseEndedDelegate;
 
     private Text _messageText;
+    private BasePopupComponent _basePopupComponent;
 
     public AlertPopUp Initialize()
     {
         _childrenController = gameObject.AddComponent<ChildrenControllerComponent>();
+        _basePopupComponent = gameObject.GetComponent<BasePopupComponent>();
+        _onCloseEndedDelegate = OnCloseEnded;
+        _basePopupComponent.Initialize(_onCloseEndedDelegate);
         _messageText = GetComponentsInChildren<Text>(true)[0];
         return this;
     }
@@ -18,13 +23,19 @@ public class AlertPopUp : MonoBehaviourEx, IHandle<ShowAlertMessage>
     public void Handle(ShowAlertMessage message)
     {
         _childrenController.EnableChildren();
+        _basePopupComponent.ShowPopUp();      
         _messageText.text = message.Message;
         return;
     }
 
-    public void ClosePopUp()
+    private void OnCloseEnded()
     {
         _childrenController.DisableChildren();
+    }
+
+    public void ClosePopUp()
+    {
+        _basePopupComponent.ClosePopUp();
         return;
     }
 }
