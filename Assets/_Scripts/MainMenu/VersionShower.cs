368b36c1ebfc61abc452de1c5ad842ce347a558f
Marcel Barros Aparici <barros.marcel92@gmail.com>
2016-01-22 21:42:02 +0100

    (feat) added version number

---
+++ Assets/_Scripts/MainMenu/VersionShower.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Assets.Scripts.VersionEditor;
+using UnityEngine.UI;
+
+namespace Assets._Scripts.MainMenu
+{
+    public class VersionShower: MonoBehaviour
+    {
+        private Text _versionText;
+        void Start()
+        {
+            _versionText = GetComponent<Text>();
+            _versionText.text = VersionInformation.ToString();
+        }
+    }
+}
