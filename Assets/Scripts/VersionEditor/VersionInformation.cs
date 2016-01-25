f957070b675d2d67cb736a563fcc27163ff94c75
Marcel Barros Aparici <barros.marcel92@gmail.com>
2016-01-25 21:00:07 +0100

    (feat) enable untiy analytics

--- Assets/Scripts/VersionEditor/VersionInformation.cs
+++ Assets/Scripts/VersionEditor/VersionInformation.cs
@@ -1,12 +1,12 @@
-/*Auto-Generated File from VersionEditor 2016.01.22 21:34:00 */
+/*Auto-Generated File from VersionEditor 2016.01.25 20:59:20 */
 
 namespace Assets.Scripts.VersionEditor {
 	public static class VersionInformation {
 		public new static string ToString() {
 			return string.Format("{0}.{1}.{2}.{3}", Major, Minor, Revision, Build);
 		}
-		public const int Major = 1;
-		public const int Minor = 0;
+		public const int Major = 0;
+		public const int Minor = 9;
 		public const int Revision = 0;
 		public const string Build = "0";
 
