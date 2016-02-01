6191d05d7cfe6f75e651e8180716cef3b1f33b61
Alberto <alberto.monte.gar@gmail.com>
2016-02-01 19:49:50 +0100

    (fix) FastWave start when are 0 Roaches in Scene

--- Assets/_Scripts/Core/WaveManager.cs
+++ Assets/_Scripts/Core/WaveManager.cs
@@ -73,7 +73,7 @@ public class WaveManager : MonoBehaviourEx
                 _pizzaCount--;
                 break;
         }
-        if (CurrentElementCount() == 0)
+        if (_roachCount == 0)
         {
             _spaceRoaches.FasterWaveCycle();
         }
@@ -99,10 +99,10 @@ public class WaveManager : MonoBehaviourEx
         return this;
     }
 
-    private int CurrentElementCount()
+    /*private int CurrentElementCount()
     {
         return _roachCount + _spikeBallCount + _pizzaCount;
-    }
+    }*/
 
     private EntityWeight RandomWeightedChooser(List<EntityWeight> entityWeights)
     {
